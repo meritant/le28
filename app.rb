fff06810b28133207a5857e1160f615015e07297
User User <useruser@192.168.1.112>
2024-05-17 14:15:56 +0300

    Setup form and db

--- app.rb
+++ app.rb
@@ -2,7 +2,29 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'blogg.db'
+	@db.results_as_hash = true
+
+end
+
+before	do
+	init_db
+end
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/new' do
+	erb :new
+end
+
+post '/new' do
+	@post = params[:post]
+
+	erb  "You wrote #{@post}"
 end
